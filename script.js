0399606ec2f9c1ed85f9e0760ff5ce3547764858
christiane gozza <72118415+chritianegozza@users.noreply.github.com>
2022-05-08 01:05:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,26 @@
-// show a message with a type of the input
-function showMessage(input, message, type) {
-	// get the small element and set the message
-	const msg = input.parentNode.querySelector("small");
-	msg.innerText = message;
-	// update the class for the input
-	input.className = type ? "success" : "error";
-	return type;
+const  form = document.getElementById('form')
+
+form.addEventListener('submit', (e) => {
+e.preventDefault();
+let nome = document.getElementById('nome').value;
+let email = document.getElementById('email').value;
+let data = {
+    nome,
+    email,
 }
+let convertData = JSON.stringify(data);
 
-function showError(input, message) {
-	return showMessage(input, message, false);
-}
+localStorage.setItem('lead', convertData)
 
-function showSuccess(input) {
-	return showMessage(input, "", true);
-}
+let content = document.getElementById('content')
 
-function hasValue(input, message) {
-	if (input.value.trim() === "") {
-		return showError(input, message);
-	}
-	return showSuccess(input);
-}
+let carregando = `<p> carregando ...</p>`
 
-function validateEmail(input, requiredMsg, invalidMsg) {
-	// check if the value is not empty
-	if (!hasValue(input, requiredMsg)) {
-		return false;
-	}
-	// validate email format
-	const emailRegex =
-		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+let pronto = `<p>Usuário logado com sucesso!</p>`
+content.innerHTML = carregando
 
-	const email = input.value.trim();
-	if (!emailRegex.test(email)) {
-		return showError(input, invalidMsg);
-	}
-	return true;
-}
-
-const form = document.querySelector("#signup");
-
-const NAME_REQUIRED = "Please enter your name";
-const EMAIL_REQUIRED = "Please enter your email";
-const EMAIL_INVALID = "Please enter a correct email address format";
-
-form.addEventListener("submit", function (event) {
-	// stop form submission
-	event.preventDefault();
-
-	// validate the form
-	let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
-	let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
-	// if valid, submit the form.
-	if (nameValid && emailValid) {
-		alert("Cadastrado com sucesso!");
-	}
-});
+setTimeout(() => {
+    content.innerHTML = pronto
+}, 1000)
+})
 
